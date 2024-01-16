6fdd53e38b7f13632989e5969200b430ce556567
goceusdet <gocesdet@gmail.com>
2024-01-16 13:12:19 -0500

    made page instance/object a global variable

--- features/step_definitions/test_steps.rb
+++ features/step_definitions/test_steps.rb
@@ -1,16 +1,18 @@
 require_relative '../pages/stryker_main_page' #--> by requiring this we get to get the instances later 
 
+$stryker_page = StrykerMain.new #--> creating the instance
+
 Given('I visit {string}') do |string|
-    @stryker_page = StrykerMain.new #--> creating the instance
+    
     # need method that takes us to the url
-    @stryker_page.visit_url string
+    $stryker_page.visit_url string
     #visit string
 end
   
 
 When('I search for {string}') do |string|
     
-    @stryker_page.search_item(string)
+    $stryker_page.search_item(string)
 
     # find("//input[@id='q']").click #--> clicks on top search bar
     # find("//input[@id='q']").set(string).send_keys(:enter) #--> .set = sendKeys in Java && send_keys = Keys Enum in Java.
