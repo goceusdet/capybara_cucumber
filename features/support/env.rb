47a427aef2502f70f51704cbda0a0ff033a69c71
goceusdet <gocesdet@gmail.com>
2024-01-16 12:15:35 -0500

    added --start-maximized option under env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -9,7 +9,8 @@ require_relative '../pages/base_page'
 
 # High level instantiation
 chrome_options = Selenium::WebDriver::Chrome::Options.new
-chrome_options.add_argument("--window-size=1920,1080")
+#chrome_options.add_argument("--window-size=1920,1080")
+chrome_options.add_argument('--start-maximized')
 
 Capybara.register_driver :chrome do |app|
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: chrome_options)
